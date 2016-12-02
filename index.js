c9869781727d725a963f2bc7fa90036380d7e68e
E-stein <eloise.stein@outlook.fr>
2016-12-02 06:36:19 +0100

    ca marche

--- index.js
+++ index.js
@@ -49,13 +49,6 @@ var loger=/(loger|logement|logements|a la rue|maison|appartement|foyer)/i;
 var aide=/(besoin|aide|)/i;
 
 
-var pasTrouve = [
-    "je n'ai pas compris... :(",
-    "je n'ai pas pu comprendre votre demande... Pouvez-vous la reformuler ?",
-    "je n'ai pas saisir votre demande... Serait-il possible que vous la formulez d'une autre manière ?"
-];
-
-
 bot.dialog('/', [
     function (session) {
         session.beginDialog('/Dialogue', session.userData.profile);
@@ -63,9 +56,8 @@ bot.dialog('/', [
     function (session, results) {
         session.userData.profile = results.response;
         if(session.userData.profile.demande) {
-            session.send("%(name)s, voici ce que j'ai trouvé pour vous : %(demande)s", session.userData.profile);
+            session.send("%(name)s, %(demande)s", session.userData.profile);
         } else {
-            session.send("%(name)s" + pasTrouve[Math.floor(Math.random()*pasTrouve.length)], session.userData.profile);
             session.beginDialog('/Dialogue', session.userData.profile);
         }
     }
@@ -129,19 +121,19 @@ bot.dialog('/Dialogue', [
             // Si il souhaite manger
             if(results.response.match(nourriture)) {
                 var resto = data[Math.floor(Math.random()*data.length)];
-                session.dialogData.profile.demande = "\n\n Je peux vous conseiller un Resto du Coeur qui pourra vous aider !  ";
+                session.dialogData.profile.demande = "\n\n je peux vous conseiller un Resto du Coeur qui pourra vous aider !  ";
                 session.dialogData.profile.demande += "\n\n Rendez-vous au " + resto.nom;
                 session.dialogData.profile.demande += "\n\n      " + resto.adresse;
             }
 
             // S'il souhaite se loger
             else if(results.response.match(loger)) {
-                session.dialogData.profile.demande = "Je vous conseille d'appeler le 115, ils pourront vous aider rapidement !";
+                session.dialogData.profile.demande = "\n\n je vous conseille d'appeler le 115, ils pourront vous aider rapidement !";
             }
 
             // Si on a pas compris..
             else {
-                session.dialogData.profile.demande = false;
+                session.dialogData.profile.demande = " je n'ai pas pu saisir votre demande... Serait-il possible que vous la formulez d'une autre manière ?";
             }
         }
         
